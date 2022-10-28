111edfa8a4aa4c2a6d92f8fe27c0a80c921b9cd9
ekenecf <ekens2u@gmail.com>
2022-10-28 18:40:30 +0100

    Fix linters

--- src/App.js
+++ src/App.js
@@ -5,11 +5,11 @@ import Home from './components/Home';
 function App() {
   return (
     <Router>
-        <Header />
-        <Routes>
-          <Route path="/" element={<Home />} />
-        </Routes>
-      </Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
   );
 }
 
